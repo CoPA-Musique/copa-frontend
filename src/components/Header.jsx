563f699eaee62c52a15df0b0f14926c77a33324d
Soushi888 <info@sacha-pignot.com>
2020-12-11 20:00:33 -0500

    Ajout d'un dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../img/CoPA-Logo-Rouge.png";
 
@@ -10,10 +10,23 @@ export default function Header() {
         <img src={logo} alt="Logo Copa" width={100} />
       </Link>
       <Nav className="mx-auto">
-        <Nav.Link><Link to="/">Accueil</Link></Nav.Link>
-        <Nav.Link><Link to="/about">À propos</Link></Nav.Link>
-        <Nav.Link><Link to="/tarifs">Tarifs</Link></Nav.Link>
-        <Nav.Link><Link to="/faq">FAQ</Link></Nav.Link>
+        <Nav.Link>
+          <Link to="/">Accueil</Link>
+        </Nav.Link>
+        <NavDropdown title="À propos" id="basic-nav-dropdown">
+          <NavDropdown.Item>
+            <Link to="/about">À propos</Link>
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <Link to="/conditions">Conditions générales d'utilisation</Link>
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link>
+          <Link to="/tarifs">Tarifs</Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/faq">FAQ</Link>
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
