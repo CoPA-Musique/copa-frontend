53e5e5d8611dbaf481eaf997bc8fffc8c00f4279
Soushi888 <info@sacha-pignot.com>
2020-12-16 14:56:07 -0500

    correction dropdown responsive

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import style from "./DropdownMenu.module.css";
 
 const DropdownMenu = ({ title, children }) => {
+  const toggleDropdown = () => {
+    const dropdownContent = document.querySelector(
+      `.${style["dropdown-content"]}`
+    );
+
+    if (!dropdownContent.classList.contains(style.responsive)) {
+      dropdownContent.classList.add(style.responsive);
+    } else {
+      dropdownContent.classList.remove(style.responsive);
+    }
+  };
+
   return (
     <li className={style.dropdown}>
-      <button>
+      <button onClick={toggleDropdown}>
         {title} <span className={style.small}>&#9660;</span>
       </button>
       <ul className={style["dropdown-content"]}>{children}</ul>
