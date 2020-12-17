f80900a36b38020e34bfe12d22f786728e04eec0
Soushi888 <info@sacha-pignot.com>
2020-12-16 21:07:40 -0500

    Création Footer

--- src/components/Footer/Copyright/Copyright.jsx
+++ src/components/Footer/Copyright/Copyright.jsx
@@ -4,10 +4,7 @@ import style from "./Copyright.module.css";
 const Copyright = () => {
   return (
     <div className={style.copyright}>
-      <p>
-        &copy;2020 Coop de travail pour les professionnels autonomes de la
-        Musique (CoPA Musique). Tout droits réservés.
-      </p>
+      <p>&copy;2020 CoPA Musique | Tout droits réservés.</p>
     </div>
   );
 };
