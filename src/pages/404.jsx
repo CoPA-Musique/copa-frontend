de2a7e24116af5261390fd754689a49edb39c746
Soushi888 <info@sacha-pignot.com>
2020-12-17 22:54:59 -0500

    Création page erreur 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error404  = () => {
+    return (
+        <main>
+            <h1>Erreur 404, page non trouvée.</h1>
+        </main>
+    )
+}
+
+export default Error404;
