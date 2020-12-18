71003ef469ed4175658df3c16619634496e5fd5e
Soushi888 <info@sacha-pignot.com>
2020-12-17 23:45:06 -0500

    Création de la composante SiteMap

---
+++ src/components/Footer/SiteMap/SiteMap.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./SiteMap.module.css";
+
+const SiteMap = () => {
+  return (
+    <div className={style.siteMap}>
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/about"> À propos</Link>
+          <ul>
+            <li>
+              <Link to="/conditions-utilisation">Condition d'utilisation</Link>
+            </li>
+            <li>
+              <Link to="/politiques-confidentialite">
+                Politiques de confidentialité
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/tarifs">Tarifs</Link>{" "}
+        </li>
+      </ul>
+      
+      <ul className={style["siteMap__liste2"]}>
+        <li>
+          <Link to="/comment-ca-marche">Comment ça marche</Link>
+        </li>
+        <li>
+          <Link to="/nouvelles">Nouvelles</Link>
+        </li>
+        <li>
+          <Link to="/contact">Nous joindre</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SiteMap;
