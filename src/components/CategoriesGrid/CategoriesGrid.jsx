a3af856f7d36f16366072d1f3e48bf8559036e39
Soushi888 <info@sacha-pignot.com>
2020-12-21 16:21:47 -0500

    Création composante CategoriesGrid

---
+++ src/components/CategoriesGrid/CategoriesGrid.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import style from "./CategoriesGrid.module.css";
+import musicIllustration from "../../img/free-music-vector-icon.jpeg";
+
+const CategoriesGrid = () => {
+  return (
+    <section className={style.CategoriesGrid}>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+      <div>
+        <img
+          src={musicIllustration}
+          alt="illustrations instruments de musiques"
+        />
+        <h3>Catégorie</h3>
+      </div>
+    </section>
+  );
+};
+
+export default CategoriesGrid;
