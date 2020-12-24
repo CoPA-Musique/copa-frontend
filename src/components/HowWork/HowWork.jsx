561ed33c1cb7d50128decacdd112c3b099b86eb9
Soushi888 <info@sacha-pignot.com>
2020-12-23 22:47:41 -0500

    supression temporaire du useState

--- src/components/HowWork/HowWork.jsx
+++ src/components/HowWork/HowWork.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import style from "./HowWork.module.css";
 
 const HowWork = () => {
-  const [section, setSection] = useState({
-    "sous-titre": "Visiter",
-    prix: "gratuit",
-    text:
-      "Accès au répertoire d’abonnés et à leur page de présentation personnelle agissant comme véritable point de rencontre entre les professionnels de la scène artistique régionale, CoPA Musique permet à tous d’accéder à notre liste d’abonnés. Grâce à notre outil de recherche efficace et intuitif, il sera facile de trouver un professionnel ou encore, un groupe de musique correspondant à vos besoins.",
-  });
+  // const [section, setSection] = useState({
+  //   "sous-titre": "Visiter",
+  //   prix: "gratuit",
+  //   text:
+  //     "Accès au répertoire d’abonnés et à leur page de présentation personnelle agissant comme véritable point de rencontre entre les professionnels de la scène artistique régionale, CoPA Musique permet à tous d’accéder à notre liste d’abonnés. Grâce à notre outil de recherche efficace et intuitif, il sera facile de trouver un professionnel ou encore, un groupe de musique correspondant à vos besoins.",
+  // });
 
   return (
     <div className={style.HowWork}>
