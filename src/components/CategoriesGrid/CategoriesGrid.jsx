863dd3fe1a5307278bf89df8c703f60242521102
Soushi888 <info@sacha-pignot.com>
2020-12-23 22:46:00 -0500

    Intégration composante CategoryCell + mode responsive

--- src/components/CategoriesGrid/CategoriesGrid.jsx
+++ src/components/CategoriesGrid/CategoriesGrid.jsx
@@ -1,94 +1,83 @@
 import React from "react";
 import style from "./CategoriesGrid.module.css";
 import musicIllustration from "../../img/free-music-vector-icon.jpeg";
+import CategoryCell from "./CategoryCell/CategoryCell";
 
 const CategoriesGrid = () => {
   return (
     <section className={style.CategoriesGrid}>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
-      <div>
-        <img
-          src={musicIllustration}
-          alt="illustrations instruments de musiques"
-        />
-        <h3>Catégorie</h3>
-      </div>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
+      <CategoryCell
+        src={musicIllustration}
+        alt="illustrations instruments de musiques"
+      >
+        Catégorie
+      </CategoryCell>
     </section>
   );
 };
