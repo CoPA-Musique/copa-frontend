25cf64dcd43295b3fc66dfc823895d875dd2a71e
Soushi888 <info@sacha-pignot.com>
2020-12-25 21:53:59 -0500

    création composante TableauComparatif

---
+++ src/components/TableauComparatifs/TableauComparatifs.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import style from "./TableauComparatifs.module.css";
+
+const TableauComparatifs = () => {
+  return (
+    <div className={style.TableauComparatifs}>
+      <h2>Tableau comparatifs</h2>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Membres gratuits</th>
+              <th>Membres Pros</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Lorem Ipsum</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>Lorem Ipsum</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>Lorem Ipsum</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>Lorem Ipsum</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <td>Lorem Ipsum</td>
+              <td>X</td>
+              <td>X</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TableauComparatifs;
