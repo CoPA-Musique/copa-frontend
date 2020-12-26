425d28983b972613af284bb1deacfd4b74775e6e
Soushi888 <info@sacha-pignot.com>
2020-12-25 19:35:21 -0500

    Ajout Logo jaune au Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import style from "./Hero.module.css";
+import logo_jaune from "../../img/COPA-logo-jaune.png";
 
 const Hero = ({ title, children, btn_text, btn_link }) => {
   return (
@@ -10,6 +11,9 @@ const Hero = ({ title, children, btn_text, btn_link }) => {
       <hr />
       <div className={style.hero__btn}>
         <Button btn_link="/about" btn_text="À propos" />
+      </div>
+      <div className={style.logo}>
+        <img src={logo_jaune} alt="Logo de CoPA Musique" />
       </div>
     </section>
   );
