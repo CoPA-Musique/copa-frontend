b2a57ef4fd77f9e0eacbea03e976d64c5874efef
Soushi888 <Sacha.pignot@gmail.com>
2021-01-24 16:00:59 -0500

    Intégration contenu dumb

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,83 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import Heading from "../../components/atoms/Heading/Heading";
+import Text from "../../components/atoms/Text/Text";
 
 const About = () => {
   useEffect(() => {
-    document.title = "À propos - CoPA Musique"
+    document.title = "À propos - CoPA Musique";
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <main>
-      <h1>À propos</h1>
+      <Heading>À propos</Heading>
+      <Text>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+        laudantium autem amet, architecto blanditiis ex placeat dolorem itaque
+        culpa, illo ducimus, soluta ab? Quibusdam quo optio accusamus nemo
+        facilis laboriosam!
+      </Text>
+      <Heading type={2}>Lorem Ipsum...</Heading>
+      <Text>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+        laudantium autem amet, architecto blanditiis ex placeat dolorem itaque
+        culpa, illo ducimus, soluta ab? Quibusdam quo optio accusamus nemo
+        facilis laboriosam!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+      </Text>
+      <Heading type={2}>Lorem Ipsum...</Heading>
+      <Text>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+        laudantium autem amet, architecto blanditiis ex placeat dolorem itaque
+        culpa, illo ducimus, soluta ab? Quibusdam quo optio accusamus nemo
+        facilis laboriosam!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+      </Text>
+      <Heading type={2}>Lorem Ipsum...</Heading>
+      <Text>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+        laudantium autem amet, architecto blanditiis ex placeat dolorem itaque
+        culpa, illo ducimus, soluta ab? Quibusdam quo optio accusamus nemo
+        facilis laboriosam!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+        laboriosam laudantium corrupti distinctio odio quia officia maiores
+        harum, voluptate iste ratione optio minus ut accusamus, quae commodi
+        illo iure necessitatibus!
+      </Text>
     </main>
   );
 };
--- src/pages/About/ConditionsUtilisation.jsx
+++ src/pages/About/ConditionsUtilisation.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import Heading from "../../components/atoms/Heading/Heading";
+import Text from "../../components/atoms/Text/Text";
+import Contract from "../../components/organismes/Contract/Contract";
 
 const ConditionsUtilisation = () => {
   useEffect(() => {
@@ -8,7 +11,117 @@ const ConditionsUtilisation = () => {
 
   return (
     <main>
-      <h1>Conditions générales d'utilisation</h1>
+      <Heading>Conditions générales d'utilisation</Heading>
+      <Contract>
+        <Heading type={2}>1. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+        <Heading type={2}>2. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+        <Heading type={2}>3. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+        <Heading type={2}>4. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+        <Heading type={2}>5. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+        <Heading type={2}>6. Lorem Ipsum</Heading>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, a
+          aliquid. Tempore sunt architecto modi blanditiis, sequi asperiores
+          consequatur! Sapiente distinctio recusandae praesentium reprehenderit
+          facilis commodi inventore in nihil quo?
+          <br />
+        </Text>
+      </Contract>
     </main>
   );
 };
