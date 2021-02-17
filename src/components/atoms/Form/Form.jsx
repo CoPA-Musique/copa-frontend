efdb474f0238406b25a3823c17a882df817e4d81
Soushi888 <Sacha.pignot@protonmail.com>
2021-02-17 07:47:57 -0500

    onSubmit event

--- src/components/atoms/Form/Form.jsx
+++ src/components/atoms/Form/Form.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import style from "./Form.module.css";
 
-const Form = ({method, action, children}) => {
+const Form = ({method, action, children, onSubmit}) => {
     return (
-        <form className={style.Form} method={method} action={action}>
+        <form className={style.Form} onSubmit={onSubmit} method={method} action={action}>
             {children}
         </form>
     );
