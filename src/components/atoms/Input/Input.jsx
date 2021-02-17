9c9d910229c93a87d91ebb205834f87c9519f455
Soushi888 <Sacha.pignot@protonmail.com>
2021-02-17 07:47:40 -0500

    input vert quand valide

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import style from "./Input.module.css";
 
-const Input = ({label, type = "text", name, isRequired = false, inputHandler, value}) => {
+const Input = ({label, type = "text", name, isRequired = false, isValid = false, inputHandler, value}) => {
+    console.log(name, isValid);
+    console.log(`${style.Input} ${isValid ? style["is-valid"] : ""}`)
+
     return (
-        <div className={style.Input}>
+        <div className={`${style.Input} ${isValid ? style["is-valid"] : ""}`}>
             <label htmlFor={name}>{label} : </label>
             <input type={type} id={name} name={name} required={isRequired} value={value}
                    onChange={inputHandler}  />
