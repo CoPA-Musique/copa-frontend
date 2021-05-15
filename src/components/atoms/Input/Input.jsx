c2120bc0ee09811e4e96f7805f3439bf625dc3a3
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-15 01:53:29 -0400

    suppression console.log()

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import style from "./Input.module.css";
 
 const Input = ({label, type = "text", name, isRequired = false, isValid = false, inputHandler, value}) => {
-    console.log(name, isValid);
-    console.log(`${style.Input} ${isValid ? style["is-valid"] : ""}`)
+  // console.log(name, isValid);
+  // console.log(`${style.Input} ${isValid ? style["is-valid"] : ""}`)
 
     return (
         <div className={`${style.Input} ${isValid ? style["is-valid"] : ""}`}>
