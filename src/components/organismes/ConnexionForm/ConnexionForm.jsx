773a2648ff869c3a6d64c86da3d7b583a369c004
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-15 01:53:35 -0400

    formattage

--- src/components/organismes/ConnexionForm/ConnexionForm.jsx
+++ src/components/organismes/ConnexionForm/ConnexionForm.jsx
@@ -10,75 +10,76 @@ import ErrorText from "../../atoms/Text/ErrorText/ErrorText";
 
 // TODO : corriger bug de comportement
 const ConnexionForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState({email: "", password: ""});
-    const [emailValid, setEmailValid] = useState(false);
-    const [passwordValid, setPasswordValid] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({email: "", password: ""});
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
 
-    // console.log("ConnexionForm rendered")
+  // console.log("ConnexionForm rendered")
 
-    const changeEmailHandler = (evt) => {
-        setEmail(evt.target.value);
+  const changeEmailHandler = (evt) => {
+    setEmail(evt.target.value);
 
-        if (isEmail(email)) {
-            setEmailValid(true);
-            setError({...error, email: ""});
-        } else {
-            setEmailValid(false);
-            // setError({...error, email: "Adresse email invalide"});
-        }
+    if (isEmail(email)) {
+      setEmailValid(true);
+      setError({...error, email: ""});
+    } else {
+      setEmailValid(false);
+      // setError({...error, email: "Adresse email invalide"});
+    }
+  }
+
+  const changePasswordHandler = (evt) => {
+    setPassword(evt.target.value);
+
+    if (isStrongPassword(password, {minLength: 8})) {
+      setPasswordValid(true);
+      setError({...error, password: ""});
+    } else {
+      setPasswordValid(false);
+      // setError({...error, password: "Mot de passe pas assez fort"});
+    }
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!isEmail(email)) {
+      setError({...error, email: "Adresse email invalide"});
+    } else {
+      setError({...error, email: ""});
     }
 
-    const changePasswordHandler = (evt) => {
-        setPassword(evt.target.value);
-
-        if (isStrongPassword(password, {minLength: 8})) {
-            setPasswordValid(true);
-            setError({...error, password: ""});
-        } else {
-            setPasswordValid(false);
-            // setError({...error, password: "Mot de passe pas assez fort"});
-        }
+    if (!isStrongPassword(password)) {
+      setError({...error, password: "Mot de passe pas assez fort"});
+    } else {
+      setError({...error, password: ""});
     }
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+    console.log("Form submitted")
+  }
 
-        if (!isEmail(email)) {
-            setError({...error, email: "Adresse email invalide"});
-        } else {
-            setError({...error, email: ""});
-        }
+  console.log(error)
 
-        if (!isStrongPassword(password)) {
-            setError({...error, password: "Mot de passe pas assez fort"});
-        } else {
-            setError({...error, password: ""});
-        }
-
-        console.log("Form submitted")
-    }
-
-
-    return (
-        <section className={style.connexionForm}>
-            <BlueCard>
-                <Form onSubmit={(e) => submitHandler(e)}>
-                    <Input label="Adresse email" name="email" inputHandler={(e) => changeEmailHandler(e)} value={email}
-                           isValid={emailValid}
-                           isRequired/>
-                    <ErrorText>{error.email}</ErrorText>
-                    <Input label="password" name="password" type="password"
-                           inputHandler={(e) => changePasswordHandler(e)} value={password} isValid={passwordValid}
-                           isRequired/>
-                    <ErrorText>{error.password}</ErrorText>
-                    <div className={style.submitBtn}><Button btn_text="Se connecter" click={(e) => submitHandler(e)}/>
-                    </div>
-                </Form>
-            </BlueCard>
-        </section>
-    );
+  return (
+    <section className={style.connexionForm}>
+      <BlueCard>
+        <Form onSubmit={(e) => submitHandler(e)}>
+          <Input label="Adresse email" name="email" inputHandler={(e) => changeEmailHandler(e)} value={email}
+                 isValid={emailValid}
+                 isRequired/>
+          {error && <ErrorText>{error.email}</ErrorText>}
+          <Input label="password" name="password" type="password"
+                 inputHandler={(e) => changePasswordHandler(e)} value={password} isValid={passwordValid}
+                 isRequired/>
+          {error && <ErrorText>{error.password}</ErrorText>}
+          <div className={style.submitBtn}><Button btn_text="Se connecter" click={(e) => submitHandler(e)}/>
+          </div>
+        </Form>
+      </BlueCard>
+    </section>
+  );
 }
 
 export default ConnexionForm;
